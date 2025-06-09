8b750d463629613cf0877ab071bdc32d168d3137
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-09 10:38:04 +0545

    update with database creation

---
+++ models.py
@@ -0,0 +1,52 @@
+#write a database model here
+
+from database import Base
+from sqlalchemy import Column,Integer,Boolean,String,Text,ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
+
+
+class User (Base):
+    __tablename__="user"
+    id=Column(Integer,primary_key=True)
+    Username=Column(String(25),unique=True)
+    email=Column(String(80),unique=True)
+    password=Column(Text,nullable=True)
+    is_staff=Column(Boolean,default=False)
+    is_active=Column(Boolean,default=False)
+    orders=relationship("Order",back_populates='users')
+
+
+    def __repr__(self):
+        return f"<user {self.username}"
+    
+class Order(Base):
+
+    ORDER_STATUSES=(
+        ("PENDIng",'pending'),
+        ("IN-TRANSIT", "in-transit"),
+        ("DELIVERED",'delivered')
+    )
+
+    PIZZA_SIZES=(
+        ('SMALL','small'),
+        ('MEDIUM','medium'),
+        ('LARGE','large'),
+        ('EXTRA-LARGE',"extra-large")
+
+    )
+
+
+
+
+    __tablename__="orders"
+    id=Column(Integer, primary_key=True)
+    quantity=Column(Integer,nullable=False)
+    order_status=Column(ChoiceType(choices=ORDER_STATUSES),default="PENDING")
+    pizza_size=Column(ChoiceType(choices=PIZZA_SIZES),default="SMALL")
+    User_id=Column(Integer,ForeignKey('user.id'))
+    user=relationship("User",back_populates="orders")
+
+    def __repr__(self):
+        return f'<Order{self.id}'
+
